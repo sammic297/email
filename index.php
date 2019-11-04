b42d7ac6929cf9ea6f49ed126091249a16680e81
Samuel <sammic297@gmail.com>
2019-11-04 14:52:12 +0100

    update

--- index.php
+++ index.php
@@ -37,7 +37,14 @@
      
         <!-- title and description -->   
         <h3>Signup Form</h3>
-        <p>Please enter your name and email addres to create your account</p>
+        <p>Please enter your name and email address to create your account</p>
+             
+        <?php 
+            if(isset($msg)){  // Check if $msg is not empty
+                echo '<div class="statusmsg">'.$msg.'</div>'; // Display our message and wrap it with a div with the class "statusmsg".
+            } 
+        ?>
+     
          
         <!-- start sign up form -->  
         <form action="" method="post">
